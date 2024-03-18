4eb2547ca2779f8f672e00fbc7b915758d3696e9
yiranshu <shuyiran1425@hotmail.com>
2024-03-18 14:05:12 -0400

    math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+var multiply = num1*num2
+
+
+var random = Math.ceil(Math.random())
+
+let num3 =25
+let num4 =21
+var mod = num3 % num4
+
+var max = Math.max (0,1,20)
